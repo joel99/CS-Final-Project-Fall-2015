b44facecd212b6d31410f9843be788c49ae94549
Joel Ye <jye5@stuy.edu>
2016-01-11 11:31:00 -0500

    Some more

--- Risk.java
+++ Risk.java
@@ -220,46 +220,6 @@ cardBonus - this is incremented. we'll check that later
 	}
 
 	//Load map
-	fileName = "map.txt";
-	char[][] map = new char[125][302];
-	
-	line = null;
-
-	try {
-	    FileReader fileReader = new FileReader(fileName);
-	    BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-	    int linectr = 0; //couldn't use for loop with readLine()
-	    
-	    while((line = bufferedReader.readLine()) != null) {
-                char[] charArr = line.toCharArray(); //convert to array of chars
-		for (int i = 0; i < charArr.length; i++) {
-		    map[linectr][i] = charArr[i]; //copy over to map
-		}		
-		linectr++; //increment line counter
-	    }
-	    bufferedReader.close();
-	}
-
-	catch(FileNotFoundException ex) {
-	    System.out.println("Unable to open file '" + fileName + "'");
-	}
-	catch(IOException ex) {
-	    System.out.println("Error reading file '" + fileName + "'");
-	}
-
-	String retStr = "";
-	
-	for (int lineNum = 0; lineNum < map.length; lineNum++) {
-	    for (int chNum = 0; chNum < map[lineNum].length; chNum++) {
-		if ((lineNum < 30) && (chNum < 80)) {
-		    retStr += map[lineNum][chNum];
-		}
-	    }
-	    retStr += "\n";
-	}
-
-
 	readMap("map.txt");
 
 	
