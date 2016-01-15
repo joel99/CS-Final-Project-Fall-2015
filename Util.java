5dc087985a1e8af7103ade12522a83908cd8c6f4
Joel Ye <jye5@stuy.edu>
2016-01-15 11:23:42 -0500

    Add/Transfer utility functions

---
+++ Util.java
@@ -0,0 +1,37 @@
+/*******************
+ class Util - holds constants and driver's static methods 
+ ******************/
+
+
+public class Util{
+    
+    public static int[] cardBonus = [4, 6, 8, 10, 12, 15];
+
+    public int cardBonusCtr = 0; //num sets traded in so far
+
+    public static int getCardReinforce(){
+	cardBonusCtr++;
+	if (cardBonusCtr > cardBonus.length)
+	    return (cardBonusCtr - cardBonus.length) * 5 + cardBonus[cardBonus.length - 1];
+	return cardBonus[cardBonusCtr - 1];
+    }
+
+    //Wait function, for that authentic feel.
+    public static void wait(int ms){try {Thread.sleep(ms);} catch (InterruptedException ie) {}}
+	
+    //@William - it'd be nice to make this generic, but I'm not sure how
+    private static void swap( int i, int j , Country[] arr) {
+	Country temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+    }
+	
+    public static void shuffle(Country[] arr) {
+	int len = arr.length;
+	//len - i. Elements to choose from.
+	for (int i = 0; i < len; i++){
+	    swap(i, i + (int)(Math.random()*(len-i)), arr);
+	}
+    }
+
+}
