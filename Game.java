459911dc30e8cc35e89c8349f215d44933cbd6ed
wxiang54 <wxiang@stuy.edu>
2016-01-15 10:09:15 -0500

    moved some code from Risk.java to Game.java

--- Game.java
+++ Game.java
@@ -2,5 +2,14 @@
  class Game - stores all game data. Can be saved to txt
  *********************/
 public class Game{
+    private int numPlayers = 6;
+    
+    private User[] users = new User[6];
+    for (int i = 0; i < users.length; i++) {
+	users[i] = new Player(i);
+    }
+
+
+
 
 }
